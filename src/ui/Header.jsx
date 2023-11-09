cd8ae48ce429f30c0906fe35d9ac68920716caff
LAPTOP-B09BFU02\Mirek <szulckacper19@gmail.com>
2023-11-09 21:23:27 +0100

    Updating user data and password

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,7 +1,11 @@
 import styled from "styled-components";
-import Logout from "../features/authentication/Logout";
-
+import HeaderMenu from "./HeaderMenu";
+import UserAvatar from "../features/authentication/UserAvatar";
 const StyledHeader = styled.header`
+  display: flex;
+  gap: 2.4rem;
+  justify-content: end;
+  align-items: center;
   padding: 1.2rem 4.8rem;
   background-color: var(--color-grey-0);
   border-bottom: 1px solid var(--color-grey-100);
@@ -9,7 +13,8 @@ const StyledHeader = styled.header`
 function Header() {
   return (
     <StyledHeader>
-      <Logout />
+      <HeaderMenu />
+      <UserAvatar />
     </StyledHeader>
   );
 }
