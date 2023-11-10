25ac92755421fe63e350b664cb0133e117298b20
LAPTOP-B09BFU02\Mirek <szulckacper19@gmail.com>
2023-11-10 15:19:58 +0100

    Displaying a line chart with the recharts library

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,10 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
+import Stats from "./Stats";
+import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +12,33 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { isLoading: isLoading1, bookings } = useRecentBookings();
+  const {
+    stays,
+    confirmedStays,
+    isLoading: isLoading2,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoading3 } = useCabins();
+  const cabinsCount = cabins?.length;
+  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        numDays={numDays}
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        cabinsCount={cabinsCount}
+      />
+      <div>Today activs</div>
+      <div>Chart stay durations</div>
+      <div>chart sales</div>
+      <SalesChart numDays={numDays} bookings={bookings} />
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
